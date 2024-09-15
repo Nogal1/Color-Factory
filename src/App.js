f3682ff872c3cb8940100e5d7bd21b7e2651b356
Nogal1 <mcclendon.logan1234@gmail.com>
2024-09-15 16:02:20 -0700

    added Nav.js component

--- src/App.js
+++ src/App.js
@@ -2,3 +2,4 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import ColorList from "./components/ColorList";
 import ColorDetails from "./components/ColorDetails";
+import NewColorForm from "./components/NewColorForm";
